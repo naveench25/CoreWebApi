7376b080cfce9b6f5961cdb384e1646782609f2b
naveench25 <naveenchintapalli.25@gmail.com>
2019-07-14 23:21:41 +0530

    initial commit

---
+++ CoreWebApi/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string GetName()
+        {
+            return "naveen";
+        }
+    }
+}
